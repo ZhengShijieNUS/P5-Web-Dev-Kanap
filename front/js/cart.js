395eabe4d204df12d24243b05a759b21277831d0
ZhengShijieNUS <ku382168215@gmail.com>
2023-09-21 00:11:27 +0800

    milestone9: complete delete and update quantity function of the cart feature

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,6 +10,13 @@
 const productCatalogueApi = 'http://localhost:3000/api/products'
 const cart_items_section = document.querySelector('#cart__items')
 
+/**
+ * Helper function, function to create the view on the page for each article
+ * 
+ * @param {*} itemDetailRes response from the API
+ * @param {*} itemInCart the array containing the color and its quantity
+ * @param {*} productId the product specific id
+ */
 function createCartArticleViewDOM (itemDetailRes, itemInCart, productId) {
   for (let i = 0; i < itemInCart.length; i++) {
     const imgSrc = itemDetailRes.imageUrl
@@ -104,21 +111,33 @@ function createCartArticleViewDOM (itemDetailRes, itemInCart, productId) {
   }
 }
 
+/**
+ * Helper function, to calculate the number of articles currently on the page
+ * 
+ * @returns the number of the articles currently on the page
+ */
 function calculateTotalQuantityOfArticles () {
   const articlesList = cart_items_section.getElementsByTagName('article')
   return articlesList.length
 }
 
+/**
+ * Helper function, to calculate the total cost currently on the page
+ * 
+ * @returns the total cost
+ */
 function calculateTotalPrice () {
   const totalPriceElement = document.getElementById('totalPrice')
   const articlesList = cart_items_section.getElementsByTagName('article')
 
   let sum = 0
 
-  for (let i = 0; i < articlesList.length; i++){
+  for (let i = 0; i < articlesList.length; i++) {
     const article = articlesList[i]
-    
-    const unitPrice = article.querySelectorAll('.cart__item__content__description p')[1].textContent
+
+    const unitPrice = article.querySelectorAll(
+      '.cart__item__content__description p'
+    )[1].textContent
     const quantity = article.querySelector('input').value
 
     const currentProductCost = parseInt(unitPrice) * parseInt(quantity)
@@ -126,12 +145,12 @@ function calculateTotalPrice () {
     sum += currentProductCost
   }
 
-  console.log(sum)
-  
   return sum
-
 }
 
+/**
+ * Function to update and display the number of articles and total price currently on the page
+ */
 function updateCartPrice () {
   const totalQuantityOfArticles = calculateTotalQuantityOfArticles()
   const totalCost = calculateTotalPrice()
@@ -143,34 +162,167 @@ function updateCartPrice () {
   totalPrice.textContent = totalCost
 }
 
-function showTheCartDetail () {
-  if (localStorage.length !== 0) {
-    const fetchPromises = [] // Store fetch promises in an array
+/**
+ * Helper function, invoked when the quantity stored in the local storage changes
+ * 
+ * @param {*} _id the product id
+ * @param {*} color the color selected
+ * @param {*} quantity the quantity to update
+ */
+function updateLocalStorageToSpecificQuantity (_id, color, quantity) {
+  if (localStorage.getItem(_id) !== null) {
+    const items = JSON.parse(localStorage.getItem(_id))
 
-    for (let i = 0; i < localStorage.length; i++) {
-      const _id = localStorage.key(i)
-      const itemInCart = JSON.parse(localStorage.getItem(_id))
-      const fetchPromise = fetch(productCatalogueApi + '/' + _id)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`Error,Status:${response.status}`)
-          }
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i]
 
-          return response.json()
-        })
-        .then(response => {
-          createCartArticleViewDOM(response, itemInCart, _id)
+      //If the item's color exists in the item, then update the quantity directly
+      if (color in item) {
+        item[color] = quantity
+        break
+      }
+    }
+
+    localStorage.setItem(_id, JSON.stringify(items))
+  }
+}
+
+/**
+ * Helper function, invoked when a specific color of a product stored in the local storage is removed
+ * 
+ * @param {*} _id 
+ * @param {*} color 
+ */
+function deleteFromLocalStorageForSpecificArticle(_id, color){
+  if(localStorage.getItem(_id) !== null){
+    const items = JSON.parse(localStorage.getItem(_id))
+
+    //Iterate the array, find out the specific color one and delete it from the array
+    for(let i = 0; i < items.length; i++){
+      const item = items[i]
+
+      if (color in item) {
+        items.splice(i, 1)
+        break
+      }
+    }
+
+    //Update the array to localStorage
+    localStorage.setItem(_id, JSON.stringify(items))
+
+    //if array is empty, remove the _id from the local storage
+    if(items.length === 0){
+      localStorage.removeItem(_id)
+    }
+  }
+}
+
+/**
+ * Function to update the GUI if there is quantity changes on the page
+ * @param {*} inputElements 
+ */
+function updatePageContentForQuantityChanges (inputElements) {
+  for (const element of inputElements) {
+    element.addEventListener('change', () => {
+      const article = element.closest('article')
+      const _id = article.dataset.id
+      const color = article.dataset.color
+      const quantity = parseInt(element.value)
+
+      //update localstorage
+      updateLocalStorageToSpecificQuantity(_id, color, parseInt(quantity))
+
+      //update the the total price through ajax request to partially update the page instead of reloading the full page
+      fetch(productCatalogueApi)
+        .then(() => {
+          updateCartPrice()
         })
         .catch(err => {
           console.error('Fetch error: ' + err.message)
         })
 
-      fetchPromises.push(fetchPromise)
-    }
+    })
+  }
+}
+
+/**
+ * Function to update the GUI if there is article removed from the page
+ * @param {*} deleteItemElements 
+ */
+function updatePageContentForDeleteArticle(deleteItemElements){
+  for (const element of deleteItemElements){
+    element.addEventListener('click',() => {
+      const article = element.closest('article')
+      const _id = article.dataset.id
+      const color = article.dataset.color
+
+      //delete the item from localstorage
+      deleteFromLocalStorageForSpecificArticle(_id, color)
+
+      //update the the total price and GUI through ajax request to partially update the page instead of reloading the full page
+      fetch(productCatalogueApi)
+        .then(() => {
+          cart_items_section.removeChild(article)
+          updateCartPrice()
+        })
+        .catch(err => {
+          console.error('Fetch error: ' + err.message)
+        })
+
+    })
+  }
+}
+
+/**
+ * The function to rendering the cart based on the data stored in the local storage
+ * @param {*} fetchPromises an array of promises, storing the promised api results for later usage
+ */
+function loadLocalStorageContentToCartPage(fetchPromises){
+  for (let i = 0; i < localStorage.length; i++) {
+    const _id = localStorage.key(i)
+    const itemInCart = JSON.parse(localStorage.getItem(_id))
+    const fetchPromise = fetch(productCatalogueApi + '/' + _id)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error,Status:${response.status}`)
+        }
+
+        return response.json()
+      })
+      .then(response => {
+        createCartArticleViewDOM(response, itemInCart, _id)
+      })
+      .catch(err => {
+        console.error('Fetch error: ' + err.message)
+      })
+
+    fetchPromises.push(fetchPromise)
+  }
+
+}
+
+/**
+ * Main function of cart.js, the entrance of all the functions
+ */
+function showTheCartDetail () {
+  if (localStorage.length !== 0) {
+    const fetchPromises = [] // Store fetch promises in an array
+
+    loadLocalStorageContentToCartPage(fetchPromises)
 
     // Use Promise.all to wait for all fetch promises to resolve
     Promise.all(fetchPromises).then(() => {
       updateCartPrice() // Call updateCartPrice() after all fetch requests are completed
+
+      const inputElements = cart_items_section.querySelectorAll('input')
+      const deleteItemElements = cart_items_section.querySelectorAll('.deleteItem')
+
+      // Update the content if there is any quantity changes
+      updatePageContentForQuantityChanges(inputElements)
+
+      // Update the content if there is any item deleted
+      updatePageContentForDeleteArticle(deleteItemElements)
+
     })
   }
 }
